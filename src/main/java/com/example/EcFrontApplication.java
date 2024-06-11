516328ad90eea9b6eb4c7561e2580612a9dc2aad
root <root@noy.localdomain>
2024-06-11 15:28:39 +0900

    first commit

---
+++ src/main/java/com/example/EcFrontApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcFrontApplication.class, args);
+	}
+
+}
